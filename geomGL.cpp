2db34614859038e43cade1335cbd026859c02af9
narussable <narussable@gmail.com>
2022-02-17 13:07:13 -0600

    First version of geomGL. Let's see if it's convinient

--- geomGL.cpp
+++ geomGL.cpp
@@ -13,13 +13,27 @@ objGL::objGL(geom opc,
     }
     glBegin(opc);
     for(auto it = 0; it<vl.size(); ++it) {
+        VectorND aux = VectorND{ver[it][0],ver[it][1],ver[it][2],w_persp};
         stateColour(it);
+        stateVertex(it);
     }
 }
 
-void objGL::stateColour(uint16_t it) const {
+void objGL::stateColour(uint16_t it) {
     VectorND sel_v = *(col+it);
+    isAlpha = (sel_v.dim()>3) ? true : false;
     isAlpha ? 
         glColor4f(sel_v[0],sel_v[1],sel_v[2],sel_v[3]) :
         glColor3f(sel_v[0],sel_v[1],sel_v[2]);
 }
+
+void objGL::stateVertex(uint16_t it) {
+    VectorND sel_v = *(ver+it);
+    isPersp = (sel_v.dim()>3) ? true : false;
+    isPersp ? 
+        glVertex4f(sel_v[0],sel_v[1],sel_v[2],sel_v[3]) :
+        glVertex3f(sel_v[0],sel_v[1],sel_v[2]);
+}
+
+void objGL::editPersp(double new_val) 
+{  w_persp = new_val;  }
--- geomGL.hpp
+++ geomGL.hpp
@@ -1,6 +1,5 @@
 #ifndef GEOM_GL
 #define GEOM_GL
-#define DEBUG
 
 #include "VectorND.hpp"
 #include <GL/glut.h>
@@ -10,14 +9,17 @@ enum geom {point = GL_POINTS, line = GL_LINES};
 class objGL {
     protected:
         VectorND *col, *ver;
-        bool isAlpha = false;
+        bool isAlpha = false, isPersp = false;
+        double w_persp = 1.0;
 
     public:
         objGL(geom,
               std::initializer_list<VectorND>,
               std::initializer_list<VectorND>);
         
-        void stateColour(uint16_t) const;
+        void stateColour(uint16_t);
+        void stateVertex(uint16_t);
+        void editPersp(double = 1.0);
 };
 
 #endif
